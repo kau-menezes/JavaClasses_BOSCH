fdd1a23765c931e180d9597d578dc41a1d4a1407
akauzinha47 <kau.vmenezes@gmail.com>
2024-05-03 11:42:14 -0300

    fix: login and list incrementing

--- pointregistrer/Main.java
+++ pointregistrer/Main.java
@@ -6,9 +6,7 @@ public class Main {
     
     public static void main(String[] args) {
 
-        System.out.println("\n========= BEM-VINDO AO SISTEMA DE PONTO =========");
-        
-        // instancia o scanf :^)
+        // instancia o scanf c^:
         Scanner scanf = new Scanner(System.in);
         
         // instancia uma Lista de Nós
@@ -18,53 +16,128 @@ public class Main {
         Employee admin = new Employee("Admin", "123", "456789", "11082022", 1);
         list.addEmployeeNode(admin);
 
-        // printa o Menu de Login:
-        System.out.println("\nDigite seu CPF:");
-        String cpfLogin = scanf.nextLine();
+        // cria as variáveis de login
+        String cpfLogin = null;
+        String passwordLogin = null;
 
-        System.out.println("\nDigite sua Senha:");
-        String passwordLogin = scanf.nextLine();
-
-
-        // percorre a lista atrás do dito cujo do usuário pra conferir as credenciais
-        int userAccess = list.verifyAccess(cpfLogin, passwordLogin);
-
-        // lida com os erros
-        errorHandling(userAccess);
-
-        if (userAccess == 0) {
-            System.err.println("\nVocê deseja:\n[1] Bater ponto\n[2] Fazer Logout");
-
-        } else{
-            System.err.println("\nVocê deseja:\n[1] Cadastrar novo empregado\n[2] Listar empregados\n[3] Conferir pontos\n[4] Fazer logout\n\nR: ");
-
-        }
+        int userAccess;
         
-        // previne memory leak
-        scanf.close();
-    }
-
-    public static void errorHandling(int returnInt) {
-
-        switch (returnInt) {
-            case 0:
-            System.err.println("\nUsuário logado com sucesso!");
-                break;
+        while (true)
+        {
             
-            case 1:
-            System.err.println("\nAdministrador logado com sucesso!");
-            break;
+            
+            if (cpfLogin == null || passwordLogin == null) {
+                    System.out.println("\n========= BEM-VINDO AO SISTEMA DE PONTO =========");
 
-            case 2:
-            System.err.println("\nSenha incorreta! :(");
+                    // printa o Menu de Login:
+                    System.out.println("\nDigite seu CPF:");
+                    cpfLogin = scanf.nextLine();
+        
+                    System.out.println("\nDigite sua Senha:");
+                    passwordLogin = scanf.nextLine();
+                    
+                }
 
-            case 3:
-            System.err.println("\nCPF não encontrado. :(");
+                // percorre a lista atrás do dito cujo do usuário pra conferir as credenciais
+                userAccess = list.verifyAccess(cpfLogin, passwordLogin);
+                
+                // lida com os erros
+                errorHandling(userAccess);
 
-            default:
-                break;
-        }
+                if (userAccess == 0) {
+                    System.err.print("\nVocê deseja:\n[1] Bater ponto\n[2] Fazer Logout");
+                    int choice = scanf.nextInt();
+                    scanf.nextLine();
 
-    }
+                    switch (choice) {
+                        case 1:
+                            
+                            break;
+                        
+                        case 2:
+                            cpfLogin = null;
+                            passwordLogin = null;
+                            System.out.println(("\nUsuário deslogado com sucesso."));
+                            break;
+                        
+                        default:
+                            break;
+                    }
+
+    
+                } else{
+                    System.err.print("\nVocê deseja:\n[1] Cadastrar novo empregado\n[2] Listar empregados\n[3] Conferir pontos\n[4] Fazer logout\n\nR: ");
+                    int choice = scanf.nextInt();
+                    scanf.nextLine();
+                
+                    switch (choice) {
+    
+                        case 1:
+                            System.out.println("\nDigite o nome do(a) empregado(a): ");
+                            String employeeName = scanf.nextLine();
+                            System.out.println("\nDigite o CPF do(a) empregado(a): ");
+                            String employeeCPF = scanf.nextLine();
+                            System.out.println("\nDigite a senha de acesso do(a) empregado(a): ");
+                            String employeePassword = scanf.nextLine();
+                            System.out.println("\nDigite a data de nascimento do(a) empregado(a) no formato: DDMMAAAA, com somente números: ");
+                            String employeeBirthday = scanf.nextLine();
+                            System.out.println("\nDigite [1] para torná-lo Administrador do Sistema ou [0] para torná-lo empregado. ");
+                            int employeeAcess = scanf.nextInt();
+    
+                            // instancia um novo Emprgado
+                            Employee newEmployee = new Employee(employeeName, employeeCPF, employeePassword, employeeBirthday, employeeAcess);
+                            list.addEmployeeNode(newEmployee);
+                            
+                            break;
+                        
+                        case 2:
+                            list.printEmployees();
+                            break;
+                        
+                        case 3:
+                            // ?
+                            break;
+
+                        case 4:
+                            cpfLogin = null;
+                            passwordLogin = null;
+                            System.out.println(("\nUsuário deslogado com sucesso."));
+                            break;
+    
+                        default:
+                            break;
+                    }
+                }
+            
+            }
+
+            // previne memory leak
+            // scanf.close();
+                
+            }
+            public static void errorHandling(int returnInt) {
+    
+                switch (returnInt) {
+                    case 0:
+                    System.err.println("\nUsuário logado com sucesso!");
+                        break;
+                    
+                    case 1:
+                    System.err.println("\nAdministrador logado com sucesso!");
+                    break;
+            
+                    case 2:
+                    System.err.println("\nSenha incorreta!");
+            
+                    case 3:
+                    System.err.println("\nCPF não encontrado.");
+            
+                    default:
+                        break;
+                }
+            
+            }
+
 
 }
+
--- pointregistrer/NodeList.java
+++ pointregistrer/NodeList.java
@@ -1,57 +1,63 @@
 package pointregistrer;
 
-import java.security.InvalidParameterException;
-
 public class NodeList {
-    
+    private Integer p = 0;
     private Integer size = 0;
+    
     Node head = null;
-
+    
     public NodeList() {}
-
+    
     public void addEmployeeNode(Employee value) {
-
+        
         Node newNode = new Node(value);
-        size++;
 
+        size++;
+        
         if (head == null) {
             head = newNode;
+            return;
         }
-
+        
         Node current = head;
 
         while (current.getNext() != null) {
-            current = current.getNext() ;
+            current = current.getNext();
         }
-
+        
         current.setNext(newNode);
-
+        
     }
-
+    
     public int verifyAccess(String cpf, String password) {
-
+        
         Node current = head;
         
-        do {
-
-            if (current.getValue().getCpf() == cpf) {
+        while (!current.getValue().getCpf().equals(cpf)) {
+            if (current.getValue().getCpf().equals(cpf)) {
                 break;
             }
-
-            if (current.getNext() == null) {
-                return 3;
-            }
-
             current = current.getNext();
-        } while (current.getNext() != null);
-
-        if (current.getValue().getPassword() != password) {
-            return 2;
+            
         }
-
+        
         return current.getValue().getAdmin();
-
+        
     }
 
+    public Integer getSize() {
+        return this.size;
+    }
 
+    public void printEmployees() {
+        
+        Node current = head;
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("Nome: " + current.getValue().getName() + " - Usuário: " + (current.getValue().getAdmin() == 1 ? "Admin" : "Normal"));
+            current = current.getNext();
+        }
+
+    }
+    
 }
