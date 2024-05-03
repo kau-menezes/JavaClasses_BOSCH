1cdacb8110e7b2d6880640eeebfaf04edafbfe3d
akauzinha47 <kau.vmenezes@gmail.com>
2024-05-03 11:49:51 -0300

    feat: point class and attributes

--- pointregistrer/Point.java
+++ pointregistrer/Point.java
@@ -1,5 +1,31 @@
 package pointregistrer;
 
 public class Point {
+    private Point next = null;
+    private String cpf;
+    private int hour;
+    private int minute;
+    
+    Point (String cpf, int hour, int minute) {
+        this.cpf = cpf;
+        this.hour = hour;
+        this.minute = minute;
+    }
+    
+    public Point getNext() {
+        return next;
+    }
+    public void setNext(Point next) {
+        this.next = next;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+
     
 }
