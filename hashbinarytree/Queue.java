d65e3a5ee8464a3f118fa5cd79b0719c577fc49d
akauzinha47 <kau.vmenezes@gmail.com>
2024-05-14 08:49:16 -0300

    feat: queue methods

--- hashbinarytree/Queue.java
+++ hashbinarytree/Queue.java
@@ -7,4 +7,33 @@ public class Queue {
 
     Queue() {}
 
+    public void addNode(int value) {
+
+        Node newnode = new Node(value);
+
+        if (head == null) {
+            head = newnode;
+        }
+
+        Node current =  head;
+
+        while(current.getNext() != null) {
+            current = current.getNext();
+        }
+
+        current.setNext(newnode);
+
+    }
+
+    public void popNode(int value) {
+
+        quantity--;
+
+        if (head.getNext() == null) {
+            head = null;
+        }
+
+        head = head.getNext();
+    }
+
 }
